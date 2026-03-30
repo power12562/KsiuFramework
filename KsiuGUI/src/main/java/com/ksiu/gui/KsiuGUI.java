9902aae4d6a266738f3f49698eb8250912e1bb28
power12562 <kimsiwoo1005@gmail.com>
2026-03-30 16:21:29 +0900

    chore(project):gui 프로젝트 추가

---
+++ KsiuGUI/src/main/java/com/ksiu/gui/KsiuGUI.java
@@ -0,0 +1,19 @@
+package com.ksiu.gui;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class KsiuGUI extends JavaPlugin
+{
+
+    @Override
+    public void onEnable()
+    {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable()
+    {
+        // Plugin shutdown logic
+    }
+}
