7492217526b0d32f2fb492d885313f4786efdc92
power12562 <kimsiwoo1005@gmail.com>
2026-03-31 19:52:37 +0900

    feature(core):ItemBuilder 클래스 추가

---
+++ KsiuCore/src/main/java/com/ksiu/core/builders/ItemBuilder.java
@@ -0,0 +1,59 @@
+package com.ksiu.core.builders;
+
+import net.kyori.adventure.text.Component;
+import org.bukkit.Material;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ItemBuilder
+{
+    private final ItemStack _itemStack;
+    private final ItemMeta _meta;
+
+    public ItemBuilder(Material material)
+    {
+        _itemStack = new ItemStack(material);
+        _meta = _itemStack.getItemMeta();
+    }
+
+    public ItemBuilder setName(String name)
+    {
+        return setName(Component.text(name));
+    }
+
+    public ItemBuilder setName(Component name)
+    {
+        if (_meta != null)
+            _meta.displayName(name);
+        return this;
+    }
+
+    public ItemBuilder setLore(List<Component> lore)
+    {
+        if (_meta != null)
+            _meta.lore(lore);
+        return this;
+    }
+
+    public ItemBuilder addLore(Component line)
+    {
+        if (_meta != null)
+        {
+            List<Component> lore = _meta.lore();
+            if (lore == null)
+                lore = new ArrayList<>();
+            lore.add(line);
+            _meta.lore(lore);
+        }
+        return this;
+    }
+
+    public ItemStack build()
+    {
+        _itemStack.setItemMeta(_meta);
+        return _itemStack;
+    }
+}
