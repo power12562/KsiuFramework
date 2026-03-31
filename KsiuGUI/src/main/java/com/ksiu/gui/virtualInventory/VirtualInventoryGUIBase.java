f5757247a6a74548c6eb4bc3e769d0397e7ce3ff
power12562 <kimsiwoo1005@gmail.com>
2026-03-31 19:53:50 +0900

    feature(GUI) 가상 인벤토리 GUI 베이스 클래스 추가

---
+++ KsiuGUI/src/main/java/com/ksiu/gui/virtualInventory/VirtualInventoryGUIBase.java
@@ -0,0 +1,120 @@
+package com.ksiu.gui.virtualInventory;
+
+import com.ksiu.gui.interfaces.IGUI;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.inventory.InventoryCloseEvent;
+import org.bukkit.event.inventory.InventoryOpenEvent;
+import org.bukkit.event.inventory.InventoryType;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.InventoryHolder;
+import org.bukkit.inventory.ItemStack;
+import org.jetbrains.annotations.NotNull;
+import net.kyori.adventure.text.Component;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Consumer;
+
+public abstract class VirtualInventoryGUIBase implements IGUI, InventoryHolder, Listener
+{
+    private final Inventory _inventory;
+    private final String _name;
+    private final InventoryType _type;
+    private final int _size;
+    private final Map<Integer, Consumer<InventoryClickEvent>> _actions = new HashMap<>();
+
+    public enum ESize
+    {
+        Size9(9),
+        Size18(18),
+        Size27(27),
+        Size36(36),
+        Size45(45),
+        Size54(54);
+        private final int _value;
+
+        ESize(int value)
+        {
+            _value = value;
+        }
+
+        public final int getValue()
+        {
+            return _value;
+        }
+
+        public static ESize fromInt(int value)
+        {
+            for (ESize size : values())
+            {
+                if (size._value == value)
+                    return size;
+            }
+            return null;
+        }
+    }
+
+    public VirtualInventoryGUIBase(String name, ESize eSize, Component title)
+    {
+        int size = eSize.getValue();
+        _inventory = Bukkit.createInventory(this, size, title);
+        _name = name;
+        _size = size;
+        _type = InventoryType.CHEST;
+    }
+
+    public VirtualInventoryGUIBase(String name, InventoryType type, Component title)
+    {
+        _inventory = Bukkit.createInventory(this, type, title);
+        _name = name;
+        _size = 0;
+        _type = type;
+    }
+
+    @Override
+    public final void open(Player player)
+    {
+        player.openInventory(_inventory);
+    }
+    public void onOpen(InventoryOpenEvent event) {}
+
+    @Override
+    public final void close(Player player)
+    {
+        player.closeInventory();
+    }
+    public void onClose(InventoryCloseEvent event) {}
+
+    @Override
+    public final @NotNull Inventory getInventory()
+    {
+        return _inventory;
+    }
+
+    @Override
+    public final String getName()
+    {
+        return _name;
+    }
+
+    public final void setItem(int slot, ItemStack item, Consumer<InventoryClickEvent> action)
+    {
+        _inventory.setItem(slot, item);
+        if (action != null)
+        {
+            _actions.put(slot, action);
+        }
+    }
+
+    public final void onClick(InventoryClickEvent event)
+    {
+        int slot = event.getRawSlot();
+        if (_actions.containsKey(slot))
+        {
+            _actions.get(slot).accept(event);
+        }
+    }
+}
