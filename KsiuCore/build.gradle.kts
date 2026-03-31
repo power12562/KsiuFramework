d134bd27fb2bf528e8991f76b9e98b192049c9b3
power12562 <kimsiwoo1005@gmail.com>
2026-03-31 20:47:37 +0900

    chore(project): 컴파일 옵션 변경

--- KsiuCore/build.gradle.kts
+++ KsiuCore/build.gradle.kts
@@ -5,15 +5,22 @@ plugins {
 
 repositories {
     mavenCentral()
-    maven("https://repo.papermc.io/repository/maven-public/")
+    maven(
+        "https://repo.papermc.io/repository/maven-public/"
+    )
 }
 
 dependencies {
-    compileOnly("io.papermc.paper:paper-api:1.21.11-R0.1-SNAPSHOT")
+    compileOnly(
+        "io.papermc.paper:paper-api:1.21.11-R0.1-SNAPSHOT"
+    )
 }
 
 java {
-    toolchain.languageVersion = JavaLanguageVersion.of(21)
+    toolchain.languageVersion =
+        JavaLanguageVersion.of(
+            21
+        )
 }
 
 tasks {
@@ -21,14 +28,32 @@ tasks {
         // Configure the Minecraft version for our task.
         // This is the only required configuration besides applying the plugin.
         // Your plugin's jar (or shadowJar if present) will be used automatically.
-        minecraftVersion("1.21.11")
-        jvmArgs("-Xms2G", "-Xmx2G")
+        minecraftVersion(
+            "1.21.11"
+        )
+        jvmArgs(
+            "-Xms2G",
+            "-Xmx2G"
+        )
     }
 
     processResources {
-        val props = mapOf("version" to version)
-        filesMatching("plugin.yml") {
-            expand(props)
+        val props =
+            mapOf(
+                "version" to version
+            )
+        filesMatching(
+            "plugin.yml"
+        ) {
+            expand(
+                props
+            )
         }
+    }
+
+    withType<JavaCompile> {
+        options.compilerArgs.add(
+            "-Xlint:deprecation"
+        )
     }
 }
