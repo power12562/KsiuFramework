568e4c4a28d5d21d8a5af020e1030433a677aeb6
power12562 <kimsiwoo1005@gmail.com>
2026-03-31 21:40:54 +0900

    refactor(gui): 애노테이션 추가

--- KsiuGUI/src/main/java/com/ksiu/gui/interfaces/IGUI.java
+++ KsiuGUI/src/main/java/com/ksiu/gui/interfaces/IGUI.java
@@ -1,10 +1,13 @@
 package com.ksiu.gui.interfaces;
 
 import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
 
 public interface IGUI
 {
-    void open(Player player);
-    void close(Player player);
-    String getName();
+    void open(@NotNull Player player);
+
+    void close(@NotNull Player player);
+
+    @NotNull String getName();
 }
