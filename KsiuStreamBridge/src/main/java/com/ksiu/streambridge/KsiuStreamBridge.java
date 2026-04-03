5b71066953340ef3461352a1586740d283573f7e
power12562 <kimsiwoo1005@gmail.com>
2026-04-03 11:24:26 +0900

    feature(core): StreamConnectorCommand  추가

--- KsiuStreamBridge/src/main/java/com/ksiu/streambridge/KsiuStreamBridge.java
+++ KsiuStreamBridge/src/main/java/com/ksiu/streambridge/KsiuStreamBridge.java
@@ -1,5 +1,6 @@
 package com.ksiu.streambridge;
 
+import com.ksiu.commons.streamconnector.StreamConnector;
 import com.ksiu.core.KsiuCore;
 import com.ksiu.core.commands.base.CommandBase;
 import com.ksiu.core.commands.base.OpCommandBase;
@@ -10,6 +11,8 @@ import com.ksiu.streambridge.gui.APIConnectorGUI;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.List;
 
 public final class KsiuStreamBridge extends JavaPlugin
 {
@@ -39,7 +42,7 @@ public final class KsiuStreamBridge extends JavaPlugin
         _ksiuCore.getCommandRouter().registerCommandBundle("streamBridge", _commandList);
         _commandList.put(new VersionCommand());
         _commandList.put(new ConnectCommand());
-
+        _commandList.put(new StreamConnectorCommand());
     }
 
     @Override
@@ -85,6 +88,54 @@ public final class KsiuStreamBridge extends JavaPlugin
 
             KsiuGUIStack.push(player, new APIConnectorGUI());
             return true;
+        }
+    }
+
+    private static final class StreamConnectorCommand extends OpCommandBase
+    {
+        public static String getModuleName()
+        {
+            return "streamconnector";
+        }
+
+        private final KsiuCommandList _ksiuCommandList;
+
+        public StreamConnectorCommand()
+        {
+            super(getModuleName(), "api 연동 패키지 명령어 입니다.");
+            _ksiuCommandList = new KsiuCommandList(getModuleName());
+            _ksiuCommandList.put(new VersionCommand());
+        }
+
+        @Override
+        public boolean onOpCommand(CommandSender sender, String[] args)
+        {
+            return _ksiuCommandList.onCommand(sender, args);
+        }
+
+        @Override
+        public List<String> onTabComplete(CommandSender sender, String[] args)
+        {
+            return _ksiuCommandList.onTabComplete(sender, args);
+        }
+
+        public static final class VersionCommand extends OpCommandBase
+        {
+            public VersionCommand()
+            {
+                super("version", "패키지 버전 정보입니다.");
+            }
+
+            @Override
+            public boolean onOpCommand(CommandSender sender, String[] args)
+            {
+                if (!(sender instanceof Player player))
+                    return true;
+
+                String version = StreamConnector.getVersion();
+                player.sendMessage(KsiuCore.getPrefixTextBuilder().append(version).build());
+                return true;
+            }
         }
     }
 }
