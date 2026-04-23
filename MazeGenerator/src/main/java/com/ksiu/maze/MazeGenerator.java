8ddb07554ba12be6f8e7901489ca99623f43762e
power12562 <kimsiwoo1005@gmail.com>
2026-04-23 11:11:59 +0900

    chore(project):MazeGenerator 추가

---
+++ MazeGenerator/src/main/java/com/ksiu/maze/MazeGenerator.java
@@ -0,0 +1,19 @@
+package com.ksiu.maze;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MazeGenerator extends JavaPlugin
+{
+
+    @Override
+    public void onEnable()
+    {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable()
+    {
+        // Plugin shutdown logic
+    }
+}
