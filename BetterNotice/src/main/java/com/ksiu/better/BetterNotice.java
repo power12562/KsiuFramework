78044af42d49803b3c00d2a56ecc2f2dd6ffd1c7
power12562 <kimsiwoo1005@gmail.com>
2026-04-29 19:52:54 +0900

    feature(core): Placeholder 추가 및 명령어 추가

--- BetterNotice/src/main/java/com/ksiu/better/BetterNotice.java
+++ BetterNotice/src/main/java/com/ksiu/better/BetterNotice.java
@@ -1,23 +1,44 @@
 package com.ksiu.better;
 
+import com.ksiu.better.placeholders.NoticePlaceholder;
 import kr.toxicity.hud.api.BetterHud;
 import kr.toxicity.hud.api.BetterHudAPI;
+import kr.toxicity.hud.api.manager.PlaceholderManager;
+import kr.toxicity.hud.api.placeholder.PlaceholderContainer;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.PluginCommand;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class BetterNotice extends JavaPlugin
 {
-    private BetterHud _betterHud;
 
     @Override
     public void onEnable()
     {
         // Plugin startup logic
-        _betterHud = BetterHudAPI.inst();
+        BetterHud betterHud = BetterHudAPI.inst();
+        PlaceholderManager placeholders = betterHud.getPlaceholderManager();
+        PlaceholderContainer<String> stringContainer = placeholders.getStringContainer();
+        NoticePlaceholder noticePlaceholder = new NoticePlaceholder("godlight_notice_popup");
+
+        stringContainer.addPlaceholder("better_notice", noticePlaceholder);
+        registerCommand(this, "공지", noticePlaceholder);
     }
 
     @Override
     public void onDisable()
     {
         // Plugin shutdown logic
+
+
+    }
+
+    public static void registerCommand(JavaPlugin plugin, String name, CommandExecutor executor)
+    {
+        PluginCommand cmd = plugin.getCommand(name);
+        if (cmd != null)
+        {
+            cmd.setExecutor(executor);
+        }
     }
 }
