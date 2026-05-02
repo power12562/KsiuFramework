9ddaf84f5b5ceb0a575e2d2c5cd475216e62d60b
power12562 <kimsiwoo1005@gmail.com>
2026-05-02 18:57:42 +0900

    fix(core): 공지 서식 문제 해결

--- BetterNotice/src/main/java/com/ksiu/better/mm/mechanic/BetterNoticeMechanic.java
+++ BetterNotice/src/main/java/com/ksiu/better/mm/mechanic/BetterNoticeMechanic.java
@@ -6,24 +6,30 @@ import io.lumine.mythic.api.config.MythicLineConfig;
 import io.lumine.mythic.api.skills.ITargetedEntitySkill;
 import io.lumine.mythic.api.skills.SkillMetadata;
 import io.lumine.mythic.api.skills.SkillResult;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.minimessage.MiniMessage;
+import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
 import org.bukkit.Bukkit;
 
 public class BetterNoticeMechanic implements ITargetedEntitySkill
 {
     private final BetterNotice _plugin;
+    //private final Logger _logger;
     private final String _mobNotice;
 
     public BetterNoticeMechanic(BetterNotice plugin, MythicLineConfig config)
     {
         _plugin = plugin;
+        //_logger = _plugin.getLogger();
         this._mobNotice = config.getString(new String[]{"notice", "s", "n"}, "[name]");
     }
 
     @Override
     public SkillResult castAtEntity(SkillMetadata skillMetadata, AbstractEntity abstractEntity)
     {
-        String casterName = skillMetadata.getCaster().getName();
-        String cmd = String.format("공지 %s", _mobNotice.replace("[name]", casterName));
+        Component componentName = LegacyComponentSerializer.legacySection().deserialize(skillMetadata.getCaster().getName());
+        String casterName = MiniMessage.miniMessage().serialize(componentName);
+        String cmd = String.format("공지 %s", _mobNotice.replace("[name]", casterName + "<reset>"));
         Bukkit.getScheduler().runTask(_plugin, () -> Bukkit.dispatchCommand(Bukkit.getConsoleSender(), cmd));
         return SkillResult.SUCCESS;
     }
